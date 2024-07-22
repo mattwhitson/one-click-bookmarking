622a8fc567ec47a0b3c1d27d0b9fa7cd4e17ba68
mattwhitson <174158793+mattwhitson@users.noreply.github.com>
2024-07-21 19:08:23 -0700

    prettier

--- chrome-extension/oauth.js
+++ chrome-extension/oauth.js
@@ -1,14 +1,11 @@
-chrome.runtime.sendMessage(
-    {action: 'AUTH_CHECK'},
-    (data) => {    
-        if (data) {
-            //session = data;
-            console.log("Logged in")
-        } else {
-            console.log("oops")
-            chrome.tabs.create({
-                url: 'http://localhost:3000/login'
-            }); 
-        }
-    }
-);
+chrome.runtime.sendMessage({ action: "AUTH_CHECK" }, (data) => {
+  if (data) {
+    //session = data;
+    console.log("Logged in");
+  } else {
+    console.log("oops");
+    chrome.tabs.create({
+      url: "http://localhost:3000/login",
+    });
+  }
+});
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,34 +1,36 @@
-window.onload = async function() {
-    document.querySelector('button').addEventListener('click', async function() {
-      const url = await getTabURL();                                  
-      const response = await fetch("http://localhost:3000/api/bookmarks", 
-        { 
-            method: "POST", 
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }, 
-            body: JSON.stringify({ url })
-        });
-      const data = await response.json();
-
-      if (!response.ok) {
-        console.error(data);
-      }
+window.onload = async function () {
+  document.querySelector("button").addEventListener("click", async function () {
+    const url = await getTabURL();
+    const response = await fetch("http://localhost:3000/api/bookmarks", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
     });
-  };
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.error(data);
+    }
+  });
+};
 
 // why i need to do this hacky shit just to get the URL is beyond me, but async/await would always return undefined
 function getTabURL() {
-    return new Promise((resolve, reject) => {
-        try {
-            chrome.tabs.query({
-                active: true,
-            }, function (tabs) {
-                resolve(tabs[0].url);
-            })
-        } catch (error) {
-            reject(error);
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.tabs.query(
+        {
+          active: true,
+        },
+        function (tabs) {
+          resolve(tabs[0].url);
         }
-    })
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
